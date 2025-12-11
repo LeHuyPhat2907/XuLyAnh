3144627bd05730a470b7e97bcc2f8f929a72d9d9
LeHuyPhat2907 <huyphatle2907@gmail.com>
2025-12-11 00:44:02 -0800

    step1_calibrate

---
+++ step1_calibrate/step1_calibrate.py
@@ -0,0 +1,64 @@
+import numpy as np
+import cv2
+import glob
+import pickle
+
+def run_calibration():
+    # 1. Cấu hình bàn cờ (Sửa lại theo bàn cờ thật của bạn)
+    CHESSBOARD_SIZE = (6,9)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+    # 2. Chuẩn bị tọa đồ 3D ảo
+    objp = np.zeros((CHESSBOARD_SIZE[0] * CHESSBOARD_SIZE[1],3), np.float32)
+    objp[:,:2] = np.mgrid[0:CHESSBOARD_SIZE[0], 0:CHESSBOARD_SIZE[1]].T.reshape(-1,2)
+
+    objpoints = []
+    imgpoints = []
+    
+    # 3. Đọc ảnh từ thư mục
+    images = glob.glob('calibration_images/*.jpg')
+    print(f"Đang xử lý {len(images)} ảnh...")
+
+    if len(images) == 0:
+        print("Lỗi: Không tìm thấy ảnh trong thư mục 'calibration_images'!")
+        return
+
+    gray = None
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Tìm góc
+        ret, corners = cv2.findChessboardCorners(gray, CHESSBOARD_SIZE, None)
+        print(fname, " -> ret =", ret)
+
+        if ret == True:
+            objpoints.append(objp)
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)
+
+            # (Tuỳ chọn) Vẽ để kiểm tra
+            # cv2.drawChessboardCorners(img, CHESSBOARD_SIZE, corners2, ret)
+            # cv2.imshow('Checking', img)
+            # cv2.waitKey(50)
+    
+    cv2.destroyAllWindows()
+
+    # 4. Calibrate
+    print("Đang tính toán ma trận...")
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    # 5. Xuất dữ liệu (Theo yêu cầu: Xuất camera matrix + distortion)
+    data = {
+        "camera_matrix": mtx,
+        "dist_coeff": dist,
+        "error": ret # Lưu thêm sai số để báo cáo
+    }
+
+    with open("calibration_data.pkl", "wb") as f:
+        pickle.dump(data, f)
+    
+    print(f"Hoàn tất! Đã lưu file 'calibration_data.pkl'. Sai số: {ret}")
+
+if __name__ == "__main__":
+    run_calibration()
